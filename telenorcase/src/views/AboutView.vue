0959fa4e11738c222874c5db01341aee3af1b902
Marius_Olivier <marius.olivier@revolve.no>
2024-03-18 18:53:56 +0100

    bilder og formattering

--- telenorcase/src/views/AboutView.vue
+++ telenorcase/src/views/AboutView.vue
@@ -1,16 +1,16 @@
 <template>
-   <nav class="navbar">
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
-  </nav>
   <div class="image-grid">
     <img src="../images/buckethat.jpg" alt="Description of Image 1">
     <img src="../images/lunchbox.png" alt="Description of Image 3">
     <img src="../images/cup.png" alt="Description of Image 4">
     <img src="../images/totebag.jpg" alt="Description of Image 5">
     <img src="../images/phonecover.png" alt="Description of Image 6">
-    <img src="../images/snowglobe.jpg" alt="Description of Image 7">
+    <img src="../images/snowglobe.png" alt="Description of Image 7">
+    <img src="../images/bunny-torstein.png" alt="Description of Image 8">
+    <img src="../images/bunny-marius.png" alt="Description of Image 9">
+    <img src="../images/cards-kayak.png" alt="Description of Image 10">
+    <img src="../images/socks.jpg" alt="Description of Image 11">
+    <img src="../images/tshirt.jpg" alt="Description of Image 12">
   </div>
 </template>
 
@@ -21,5 +21,21 @@
     display: flex;
     align-items: center;
   }
+}
+
+.navbar-img {
+  width: 30px; /* adjust as needed */
+  height: auto;
+}
+
+.image-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 10px;
+}
+
+.image-grid img {
+  width: 100%;
+  height: auto;
 }
 </style>
