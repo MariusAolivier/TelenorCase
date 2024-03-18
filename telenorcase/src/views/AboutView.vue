e564f4b982f4a2cd2450d50157f8370f5fcc2c19
Marius_Olivier <marius.olivier@revolve.no>
2024-03-18 18:36:25 +0100

    add images

--- telenorcase/src/views/AboutView.vue
+++ telenorcase/src/views/AboutView.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+   <nav class="navbar">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+  </nav>
+  <div class="image-grid">
+    <img src="../images/buckethat.jpg" alt="Description of Image 1">
+    <img src="../images/lunchbox.png" alt="Description of Image 3">
+    <img src="../images/cup.png" alt="Description of Image 4">
+    <img src="../images/totebag.jpg" alt="Description of Image 5">
+    <img src="../images/phonecover.png" alt="Description of Image 6">
+    <img src="../images/snowglobe.jpg" alt="Description of Image 7">
   </div>
 </template>
 
